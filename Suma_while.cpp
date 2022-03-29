7cc1bcccac008fbd53566ae12575173ac4049143
a-n-i-a <99403908+a-n-i-a@users.noreply.github.com>
2022-03-29 23:08:28 +0200

    Add files via upload

---
+++ Suma_while.cpp
@@ -0,0 +1,26 @@
+/* SPECYFIKACJA
+Dane: liczby calkowite
+Wynik: Oblicznie sumy liczb calkowitych az do wprowadzenia zera */
+
+
+#include <iostream>
+using namespace std;
+int main()
+
+{int liczba = 1, suma = 0;
+
+while (liczba > 0)
+{
+    cout << "Wprowadz liczbe: ";
+    cin >> liczba;
+    suma = suma + liczba;
+    cout << "Suma: " << suma << endl;
+}
+   {if (liczba = 0);
+    cout << "Wynik sumowania: " << suma;
+    return 0;
+   }
+
+
+return 0;
+}
